b5099c3e7ddb17bb6ead1bbd572a2314c17d2ba1
Elliott Lockwood <lockwood721@gmail.com>
2020-06-18 21:42:54 -0500

    Final Complete Code complete with README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Password-Generator
+
+Overview:
+
+This password generator is comprised of three different languages each complete on their own page - HTML, CSS(complete with media queries), and JavaScript
+
+The generate button will prompt the user with a series of questions about what 
+criteria they want in their password 
+
+They will first be required to determine the length of their password: a number between 8 and 128
+
+Then the following options include Uppercase letters, Lowercase letters, Numbers and Special Characters
+
+Once all the prompts are answered, the password will be displayed just above the generate button in a nice, clean text box
+
+--------------------------------------------------
+
+Javascript Psuedo Code:
+
+HTML correspondents 
+
+All variables and arrays listed after
+
+Event listener for the generate button will then prompt the user
+for all necessary password criteria 
+
+Alert and Return if password criteria not properly met
+
+4 IF statements pushing necessary arrays based on criteria to randomizing for loop
+
+Password returned 
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -9,28 +9,30 @@ function writePassword() {
   passwordText.value = password
 
 }
-
+// criteria variables
 var charNum;
 var charConfirm;
 var numConfirm;
 var upperConfirm;
 var lowerConfirm;
+var characterChoices = []; // empty array
 
-lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// all necessary arrays
+var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-specChar = ["\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/"];
+var specChar = ["\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/"];
 
+// event listener for the generate button
 document.getElementById("generate").addEventListener("click", function () {
-  var ps = generatePassword(); //calling password here
-  //.placeholder = ps;
+ writePassword();
 
 });
 
-
+// function created to generate the random password
 function generatePassword() {
 
   charNum = window.prompt("How many characters would you like your password to be? (number between 8 and 128)");
@@ -40,6 +42,9 @@ function generatePassword() {
     return; // the return terminates the function 
   }
 
+  //ensures clear array to begin
+  charaterChoices = [];
+  // these prompts allow user select what specific password criteria they want
   upperConfirm = window.confirm("Would you like to include Uppercase Letters?");
 
   lowerConfirm = window.confirm("Would you like to include Lowercase Letters?");
@@ -48,61 +53,56 @@ function generatePassword() {
 
   charConfirm = window.confirm("Would you like to include special characters?");
   console.log(upperConfirm, lowerConfirm, numConfirm, charConfirm)
-  
+
+  // if they don't select any they will be alerted that they must choose at least one 
   if (upperConfirm === false && lowerConfirm === false && numConfirm === false && charConfirm === false) {
     alert("MUST SELECT AT LEAST ONE!")
-    return; 
+    return;
   }
 
+  // if the user chooses upper case character 
+  // it will randomly use the uppercase character array
+  // making array available to generate password 
+  if (upperConfirm === true) {
+    characterChoices.push(upperAlpha);
+  }
+
+  // if the user chooses lower case character 
+  // it will make the lower case array available 
+  // for random selections
+  if (lowerConfirm === true) {
+    characterChoices.push(lowerAlpha);
+  }
+
+  // if the user chooses number character 
+  // it will make the numbers array available 
+  // for random selections
+  if (numConfirm === true) {
+    characterChoices.push(numbers);
+  }
+
+  // if the user chooses special characters 
+  // it will make the special characters array available 
+  // for random selections
+  if (charConfirm === true) {
+    characterChoices.push(specChar);
+  }
+
+  // initilizing the password to be empty 
+  var password = "";
+
+  // for loop to create random password of charNum
+  for (let i = 0; i < charNum; i++) {
+    // getting the index of one of the arrays that has characters that we want to use 
+    var num = Math.floor(Math.random() * characterChoices.length);
+    // charArray is a reference to one the arrays defined above 
+    var charArray = characterChoices[num];
+    // here we get the random index of the character in the charArray
+    var randomcharacterposition = Math.floor(Math.random() * charArray.length);
+    // now adding the random character to the end of the password
+    password += charArray[randomcharacterposition];
+  }
+  // returning password to be displayed 
+  return password;
 }
 
-if
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function generatePassword() {
-
-//   //var to store password generated
-
-//   var password = "";
-
-//   // prompts
-
-//   // in for loop // for loop add characters to that password
-//   // var num = Math.floor(Math.random() * 10)
-//   // var num = Math.floor(Math.random() * 26)
-//   // var randomcharacter = lowercasecharacters[num]
-//   // password += randomcharacter
-
-
-//   // all 4 arrays 
-
-
-
-//   // return 
-//   return password;
-
-
-
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
